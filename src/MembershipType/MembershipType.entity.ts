8ab81bc255d23331cb62cd5a29ccc5ecfc804865
Agustín Stringa <stringaagustin1@gmail.com>
2024-04-26 09:12:12 -0300

    adding comment to MembershipType.entity

--- src/MembershipType/MembershipType.entity.ts
+++ src/MembershipType/MembershipType.entity.ts
@@ -3,7 +3,7 @@ import crypto from "node:crypto";
 export class MembershipType {
   constructor(
     public username: string,
-    public description: string,
+    public description: string, // gym, pileta, clases
     public id = crypto.randomUUID()
   ) {}
 }
