54a9fa7aaf2bab1bf9ff51db9ba16f1aeeef3a1b
franciscag714 <franciscagramaglia714@gmail.com>
2024-05-24 00:37:24 -0300

    feat: creating CRUD-ClassType

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import { clientRouter } from "./Client/Client.routes.js";
 import { membershipType_router } from "./MembershipType/MembershipType.routes.js";
 import express from "express";
 import { Trainers_router } from "./Trainers/Trainers.routes.js";
+import { ClassType } from "./ClassType/ClassType.entity.js";
+import { classtypeRouter } from "./ClassType/ClassType.routes.js";
 
 const PORT = 3000;
 const app = express();
@@ -9,7 +11,7 @@ app.use(express.json());
 
 app.use("/api/membershiptypes", membershipType_router);
 app.use("/api/clients", clientRouter);
-
+app.use('/api/classtypes', classtypeRouter);
 
 app.use("/api/trainers", Trainers_router);
 
