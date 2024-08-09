30f44bc601bce247746ff77e476840da07e72b79
Agustín Stringa <stringaagustin1@gmail.com>
2024-08-09 10:39:30 -0300

    feat: initial approx to login auth
    bcrypt package installed. Password hashed in UPDATE and CREATE methods

--- src/Client/Client.controller.ts
+++ src/Client/Client.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { Client } from "./Client.entity.js";
 import { orm } from "../shared/db/mikro-orm.config.js";
-
+import bcrypt from "bcrypt";
 const em = orm.em;
 
 const controller = {
@@ -71,6 +71,34 @@ const controller = {
     }
   },
 
+  login: async function (req: Request, res: Response) {
+    try {
+      const client = await em.findOneOrFail(
+        Client,
+        { username: req.body.sanitizedInput.username },
+        { populate: ["progresses", "goals"] }
+      );
+      if (client != null) {
+        const auth = bcrypt.compareSync(
+          req.body.sanitizedInput.password,
+          client.password
+        );
+        if (auth) {
+          return res
+            .status(200)
+            .json({ message: "logged successfully", data: client });
+        } else {
+          return res
+            .status(200)
+            .json({ message: "username or password wrong" });
+        }
+      } else {
+        return res.status(404).json({ message: "client not found" });
+      }
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
+  },
   sanitizeClient: function (req: Request, res: Response, next: NextFunction) {
     req.body.sanitizedInput = {
       username: req.body.username,
@@ -86,6 +114,27 @@ const controller = {
       }
     });
 
+    next();
+  },
+  sanitizeLogin: function (req: Request, res: Response, next: NextFunction) {
+    req.body.sanitizedInput = {
+      username: req.body.username,
+      password: req.body.password,
+    };
+    Object.keys(req.body.sanitizedInput).forEach((key) => {
+      if (req.body.sanitizedInput[key] === undefined) {
+        delete req.body.sanitizedInput[key];
+      }
+    });
+    next();
+  },
+  hashPassword: function (req: Request, res: Response, next: NextFunction) {
+    if (req.body.sanitizedInput.password) {
+      req.body.sanitizedInput.password = bcrypt.hashSync(
+        req.body.sanitizedInput.password,
+        10
+      );
+    }
     next();
   },
 };
