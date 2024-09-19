58ab89180cfa66d0c354bd38af1ed6a64fc4647b
Elias <elias.danteo.tomas@hotmail.com>
2024-09-19 15:41:26 -0300

    feat: add findCurrentRoutine function

--- src/Routine/Routine.routes.ts
+++ src/Routine/Routine.routes.ts
@@ -1,11 +1,12 @@
-import { Router } from "express";
-import { controller } from "./Routine.controller.js";
+import { Router } from 'express';
+import { controller } from './Routine.controller.js';
 
 export const routineRouter = Router();
 
-routineRouter.get("/", controller.findAll);
-routineRouter.get("/:id", controller.findOne);
-routineRouter.post("/", controller.sanitizeRoutine, controller.add);
-routineRouter.put("/:id", controller.sanitizeRoutine, controller.update);
-routineRouter.patch("/:id", controller.sanitizeRoutine, controller.update);
-routineRouter.delete("/:id", controller.delete);
+routineRouter.get('/', controller.findAll);
+routineRouter.get('/:id', controller.findOne);
+routineRouter.post('/', controller.sanitizeRoutine, controller.add);
+routineRouter.put('/:id', controller.sanitizeRoutine, controller.update);
+routineRouter.patch('/:id', controller.sanitizeRoutine, controller.update);
+routineRouter.delete('/:id', controller.delete);
+routineRouter.get('/:userId/current', controller.findCurrentRoutine);
