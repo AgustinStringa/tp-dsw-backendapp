b239f475a0a4fbcfce79ddae5e8cb836d3c70f6c
AaronDeBernardo <142444363+AaronDeBernardo@users.noreply.github.com>
2024-09-24 12:04:41 -0300

    fix: findActiveMemberships()

--- src/Routine/CreateRoutine.controller.ts
+++ src/Routine/CreateRoutine.controller.ts
@@ -1,60 +1,35 @@
-import { Request, Response, NextFunction } from "express";
-import { orm } from "../shared/db/mikro-orm.config.js";
-import { Client } from "../Client/Client.entity.js";
+import { Request, Response } from "express";
 import { startOfDay } from "date-fns";
 import { Membership } from "../Membership/Membership.entity.js";
-import { ObjectId } from "@mikro-orm/mongodb";
+import { orm } from "../shared/db/mikro-orm.config.js";
+
 const em = orm.em;
 
 const createRoutineController = {
-  findClientsWithMemembership: async function (_: Request, res: Response) {
+  findActiveMememberships: async function (_: Request, res: Response) {
     try {
-      const clients = await em.find(
-        Client,
-        {},
-        {
-          populate: ["memberships", "memberships.type"],
-        }
-      );
       const today = startOfDay(new Date());
-      let clientsWithMembership = clients.filter((c) =>
-        c.memberships
-          .toArray()
-          .some(
-            (m) =>
-              today >= startOfDay(m.dateFrom) && startOfDay(m.dateTo) >= today
-          )
-      );
-      let clientsWithLastMembership: {
-        id: ObjectId;
-        firstName: string;
-        lastName: string;
-        email: string;
-        dni: string;
-        currentMembership: Membership;
-      }[] = [];
-      for (const c of clientsWithMembership) {
-        const currentMembership = await em.findOne(Membership, {
-          client: { $eq: c.id },
-          dateFrom: { $lte: today },
-          dateTo: { $gte: today },
-        });
 
-        if (currentMembership != null) {
-          const client = {
-            id: c._id,
-            firstName: c.firstName,
-            lastName: c.lastName,
-            email: c.email,
-            dni: c.dni,
-            currentMembership: currentMembership,
-          };
-          clientsWithLastMembership.push(client);
+      const memberships = await em.find(
+        Membership,
+        { $and: [{ dateFrom: { $lte: today } }, { dateTo: { $gt: today } }] },
+        {
+          populate: ["client", "type"],
+          fields: [
+            "dateFrom",
+            "dateTo",
+            "type.*",
+            "client.id",
+            "client.lastName",
+            "client.firstName",
+            "client.dni",
+          ],
         }
-      }
-      res.status(200).json({
-        message: "All clients with membership were found",
-        data: clientsWithLastMembership,
+      );
+
+      return res.status(200).json({
+        message: "All active memberships were found",
+        data: memberships,
       });
     } catch (error: any) {
       res.status(500).json({ message: error.message });
