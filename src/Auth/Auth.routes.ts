6623a758af8c2b07aae7b511f62b9ff1fc4790db
AaronDeBernardo <142444363+AaronDeBernardo@users.noreply.github.com>
2024-11-02 16:57:13 -0300

    feat: .env file - logout endpoint

--- src/Auth/Auth.routes.ts
+++ src/Auth/Auth.routes.ts
@@ -4,4 +4,4 @@ import { controller } from "./Auth.controller.js";
 export const authRouter = Router();
 
 authRouter.post("/", controller.sanitizeLogin, controller.login);
-//register iría aquí? o en el post de clients/trainer
+authRouter.post("/logout/", controller.sanitizeLogin, controller.logout);
