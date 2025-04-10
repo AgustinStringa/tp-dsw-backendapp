c881103bc2ad4cb3801d5fd90874e7bb58ad20bb
Agustín Stringa <stringaagustin1@gmail.com>
2025-04-10 14:45:47 -0300

    Pagos del Usuario tag documented

--- src/modules/user-payment/user-payment.routes.ts
+++ src/modules/user-payment/user-payment.routes.ts
@@ -4,6 +4,26 @@ import { Router } from "express";
 
 export const userPaymentRouter = Router();
 
+/**
+ * @swagger
+ * /api/user-payment/:
+ *   post:
+ *     summary: Inicializar un pago con la integración de Stripe
+ *     operationId: initiatePayment
+ *     tags:
+ *       - Pagos del Usuario
+ *     responses:
+ *       200:
+ *         description: Pago inicializado
+ *       401:
+ *         description: No autenticado
+ *       403:
+ *         description: Acceso denegado
+ *       404:
+ *         description: Membresía no encontrada
+ *       500:
+ *         description: Error en el servidor
+ */
 userPaymentRouter.post(
   "/",
   authMiddlewares.verifyClient,
@@ -11,6 +31,28 @@ userPaymentRouter.post(
   controller.initiatePayment
 );
 
+/**
+ * @swagger
+ * /api/user-payment/{checkoutSessionId}:
+ *   post:
+ *     summary: Procesar un pago ya iniciado con la integración de Stripe
+ *     operationId: userFullfill
+ *     tags:
+ *       - Pagos del Usuario
+ *     responses:
+ *       200:
+ *         description: Pago procesado correctamente
+ *       401:
+ *         description: No autenticado
+ *       402:
+ *         description: No se inicializó el Pago
+ *       403:
+ *         description: Acceso denegado
+ *       404:
+ *         description: Pago incializado no encontrado
+ *       500:
+ *         description: Error en el servidor
+ */
 userPaymentRouter.post(
   "/:checkoutSessionId",
   authMiddlewares.verifyClient,
